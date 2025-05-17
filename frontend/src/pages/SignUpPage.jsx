fc73d505baec064af776c2776ea4ff5894c69222
AbdelrahmanW08 <abdelrahman.wm2@gmail.com>
2025-05-16 20:23:15 -0400

    Made home page and some of sign up page

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -2,7 +2,34 @@ import React from 'react'
 
 const SignUpPage = () => {
   return (
-    <div>SignUpPage</div>
+    <div className='h-screen w-full hero-bg'>
+      <header className='max-w-6xl mx-auto flex items-center justify-between p-4'>
+
+        <Link to={"/"}>
+          <img src='/netflix-logo.png' alt='netflix-logo' className='w-52' />
+        </Link>
+
+        <div className='flex justify-center items-center mt-20 mx-3'>
+          <div className='w-full max-w-md pd-8 space-y-6 bg-black/60 rounded-lg shadow-md'>
+            <h1 className='text-center text-white text-2xl font-bold mb-4'>
+              Sign Up
+            </h1>
+            <form className='space-y-4'>
+              <div>
+                <label htmlFor='email' className='text-sm font-medium text-gray-300 block'>
+                  Email
+                </label>
+                <input type='email' 
+                  className='w-full px-3 py-2 mt-1 border border-gray-700 rounded-md bg-transparent text-white focus:outline-none focus:ring'
+                  placeholder='you@example.com'
+                  id='email'/>
+              </div>
+            </form>
+          </div>
+        </div>
+
+      </header>
+    </div>
   )
 }
 
