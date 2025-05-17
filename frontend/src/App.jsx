c472cfcd366b46bbe305242a6dc4ee4d47199bb7
AbdelrahmanW08 <abdelrahman.wm2@gmail.com>
2025-05-17 12:24:54 -0400

    Completed landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,19 @@ import { Route, Routes } from "react-router-dom"
 import HomePage from "./pages/home/HomePage.jsx"
 import LoginPage from "./pages/LoginPage.jsx"
 import SignUpPage from "./pages/SignUpPage.jsx"
+import Footer from "./components/Footer.jsx"
 
 function App() {
 
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/signup" element={<SignUpPage />} />
-      <Route path="/login" element={<LoginPage />} />
-    </Routes>
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+      <Footer />
+    </div>
   )
 }
 
