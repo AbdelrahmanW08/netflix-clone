78b30f26a647924101f4185ee1824a52b3f99d78
AbdelrahmanW08 <abdelrahman.wm2@gmail.com>
2025-05-21 17:44:00 -0400

    Completed Search and Search History

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,9 @@ import { useAuthStore } from "./store/authUser.js"
 import { useEffect } from "react"
 import { Loader } from "lucide-react"
 import WatchPage from "./pages/WatchPage.jsx"
+import SearchPage from "./pages/SearchPage.jsx"
+import HistoryPage from "./pages/HistoryPage.jsx"
+import NotFoundPage from "./pages/NotFound.jsx"
 
 function App() {
   const {user, isCheckingAuth, authCheck } = useAuthStore();
@@ -28,6 +31,9 @@ function App() {
         <Route path="/signup" element={!user ? <SignUpPage /> : <Navigate to={"/"} />} />
         <Route path="/login" element={!user ? <LoginPage /> : <Navigate to={"/"} />} />
         <Route path="/watch/:id" element={user ? <WatchPage /> : <Navigate to={"/login"} />} />
+        <Route path="/search" element={user ? <SearchPage /> : <Navigate to={"/login"} />} />
+        <Route path="/history" element={user ? <HistoryPage /> : <Navigate to={"/login"} />} />
+        <Route path="/*" element={<NotFoundPage />} />
       </Routes>
       <Footer />
       <Toaster />
